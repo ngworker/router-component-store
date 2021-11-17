3ec744d209e87729436df3b7f3b9cc512a1f57ae
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-11-17 22:34:01 +0100

    feat: fix local router store

---
+++ packages/router-component-store/src/lib/local-router-store.spec.ts
@@ -0,0 +1,155 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NavigationEnd, Router, Routes } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { filter, firstValueFrom, map, Observable, withLatestFrom } from 'rxjs';
+
+import { LocalRouterStore } from './local-router-store';
+import { RouterStore } from './router-store';
+
+@Component({
+  template: '<router-outlet></router-outlet>',
+})
+class DummyAppComponent {}
+
+@Component({
+  template: '',
+  viewProviders: [LocalRouterStore],
+})
+class DummyLoginComponent {
+  constructor(public store: LocalRouterStore) {}
+}
+
+describe(`${RouterStore.name} selectors`, () => {
+  const afterNavigated = <TValue>(
+    observable$: Observable<TValue>
+  ): Promise<TValue> =>
+    firstValueFrom(
+      router.events.pipe(
+        withLatestFrom(observable$),
+        filter((event) => event instanceof NavigationEnd),
+        map(([_, value]) => value)
+      )
+    );
+
+  const getStore = () =>
+    (
+      rootFixture.debugElement.query(By.directive(DummyLoginComponent))
+        .componentInstance as DummyLoginComponent
+    ).store;
+
+  beforeEach(async () => {
+    const routes: Routes = [
+      {
+        path: 'login',
+        children: [
+          {
+            path: ':id',
+            component: DummyLoginComponent,
+            data: { testData: 'test-data' },
+          },
+        ],
+      },
+    ];
+
+    TestBed.configureTestingModule({
+      declarations: [DummyAppComponent, DummyLoginComponent],
+      imports: [RouterTestingModule.withRoutes(routes)],
+    });
+
+    rootFixture = TestBed.createComponent(DummyAppComponent);
+    rootFixture.autoDetectChanges(true);
+
+    router = TestBed.inject(Router);
+  });
+
+  let router: Router;
+  let rootFixture: ComponentFixture<DummyAppComponent>;
+
+  it('exposes a selector for the current route', async () => {
+    await router.navigateByUrl('/login/etyDDwAAQBAJ?ref=ngrx.io#test-fragment');
+
+    const currentRoute = await firstValueFrom(getStore().currentRoute$);
+
+    expect(currentRoute).toEqual({
+      params: {
+        id: 'etyDDwAAQBAJ',
+      },
+      data: {
+        testData: 'test-data',
+      },
+      url: [
+        {
+          path: 'etyDDwAAQBAJ',
+          parameters: {},
+        },
+      ],
+      outlet: 'primary',
+      routeConfig: {
+        path: ':id',
+      },
+      queryParams: {
+        ref: 'ngrx.io',
+      },
+      fragment: 'test-fragment',
+      children: [],
+    });
+  });
+
+  it('exposes a selector for the fragment', async () => {
+    await router.navigateByUrl('/login/etyDDwAAQBAJ?ref=ngrx.io#test-fragment');
+
+    await expect(firstValueFrom(getStore().fragment$)).resolves.toBe(
+      'test-fragment'
+    );
+  });
+
+  it('exposes a selector for query params', async () => {
+    await router.navigateByUrl('/login/etyDDwAAQBAJ?ref=ngrx.io#test-fragment');
+
+    await expect(firstValueFrom(getStore().queryParams$)).resolves.toEqual({
+      ref: 'ngrx.io',
+    });
+  });
+
+  it('creates a selector for a specific query param', async () => {
+    await router.navigateByUrl('/login/etyDDwAAQBAJ?ref=ngrx.io#test-fragment');
+
+    await expect(
+      firstValueFrom(getStore().selectQueryParam('ref'))
+    ).resolves.toBe('ngrx.io');
+  });
+
+  it('exposes a selector for route params', async () => {
+    await router.navigateByUrl('/login/etyDDwAAQBAJ?ref=ngrx.io#test-fragment');
+
+    await expect(firstValueFrom(getStore().routeParams$)).resolves.toEqual({
+      id: 'etyDDwAAQBAJ',
+    });
+  });
+
+  it('creates a selector for a specific route param', async () => {
+    await router.navigateByUrl('/login/etyDDwAAQBAJ?ref=ngrx.io#test-fragment');
+
+    await expect(
+      firstValueFrom(getStore().selectRouteParam('id'))
+    ).resolves.toBe('etyDDwAAQBAJ');
+  });
+
+  it('exposes a selector for route data', async () => {
+    await router.navigateByUrl('/login/etyDDwAAQBAJ?ref=ngrx.io#test-fragment');
+
+    await expect(firstValueFrom(getStore().routeData$)).resolves.toEqual({
+      testData: 'test-data',
+    });
+  });
+
+  it('exposes a selector for the URL', async () => {
+    await router.navigateByUrl('/login/etyDDwAAQBAJ?ref=ngrx.io#test-fragment');
+
+    await expect(firstValueFrom(getStore().url$)).resolves.toBe(
+      '/login/etyDDwAAQBAJ?ref=ngrx.io#test-fragment'
+    );
+  });
+});
--- packages/router-component-store/src/lib/local-router-store.ts
+++ packages/router-component-store/src/lib/local-router-store.ts
@@ -1,26 +1,45 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRoute, ActivatedRouteSnapshot, Data, Params, Router } from '@angular/router';
-import { distinctUntilChanged, map, Observable } from 'rxjs';
+import { Inject, inject, Injectable, InjectFlags, InjectionToken } from '@angular/core';
+import { ActivatedRoute, Data, Params, Router, RouterStateSnapshot } from '@angular/router';
+import { ComponentStore } from '@ngrx/component-store';
+import { MinimalRouterStateSerializer, RouterStateSerializer, SerializedRouterStateSnapshot } from '@ngrx/router-store';
+import { map, Observable } from 'rxjs';
+
+interface LocalRouterStoreState {
+  readonly routerState: SerializedRouterStateSnapshot;
+}
 
 @Injectable()
-export class LocalRouterStore {
-  // TODO(@LayZeeDK): verify what is emitted
-  currentRoute$: Observable<ActivatedRouteSnapshot> = this.router.events.pipe(
-    map(() => this.route.snapshot),
-    distinctUntilChanged()
+export class LocalRouterStore extends ComponentStore<LocalRouterStoreState> {
+  currentRoute$ = this.select(
+    this.select((state) => state.routerState.root),
+    (route) => {
+      while (route.firstChild) {
+        route = route.firstChild;
+      }
+
+      return route;
+    }
   );
   fragment$: Observable<string | null> = this.route.fragment;
   queryParams$: Observable<Params> = this.route.queryParams;
   routeData$: Observable<Data> = this.route.data;
   routeParams$: Observable<Params> = this.route.params;
-  // TODO(@LayZeeDK): verify what is emitted
-  url$: Observable<string> = this.route.url.pipe(
-    map((urlSegments) =>
-      this.router.serializeUrl(this.router.createUrlTree(urlSegments))
-    )
-  );
+  url$: Observable<string> = this.select((state) => state.routerState.url);
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    @Inject(serializerToken)
+    private serializer: RouterStateSerializer<SerializedRouterStateSnapshot>
+  ) {
+    super({
+      routerState: serializer.serialize(router.routerState.snapshot),
+    });
+
+    this.#updateRouterState(
+      this.router.events.pipe(map(() => this.router.routerState.snapshot))
+    );
+  }
 
   selectQueryParam<TValue>(param: string): Observable<TValue> {
     return this.queryParams$.pipe(map((params) => params[param]));
@@ -29,4 +48,21 @@ export class LocalRouterStore {
   selectRouteParam<TValue>(param: string): Observable<TValue> {
     return this.routeParams$.pipe(map((params) => params[param]));
   }
+
+  #updateRouterState = this.updater<RouterStateSnapshot>(
+    (state, routerState): LocalRouterStoreState => ({
+      ...state,
+      routerState: this.serializer.serialize(routerState),
+    })
+  );
 }
+
+const serializerToken = new InjectionToken<RouterStateSerializer>(
+  'serializerToken',
+  {
+    factory: () =>
+      inject(RouterStateSerializer, InjectFlags.Optional) ??
+      new MinimalRouterStateSerializer(),
+    providedIn: 'root',
+  }
+);

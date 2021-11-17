354160ba30c99e538bcefd4133e924abb4290b7b
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-11-17 22:48:04 +0100

    refactor: move router store to router-store folder

--- packages/router-component-store/src/index.ts
+++ packages/router-component-store/src/index.ts
@@ -1,14 +1,14 @@
 // RouterStore state
-export { RouterStore } from './lib/router-store';
+export { RouterStore } from './lib/router-store/router-store';
 
 // RouterStore configuration
-export { RouterStoreModule } from './lib/router-store.module';
-export { RouterStoreConfig } from './lib/router-store-config';
+export { RouterStoreModule } from './lib/router-store/router-store.module';
+export { RouterStoreConfig } from './lib/router-store/router-store-config';
 
 // RouterStore events
-export * from './lib/router-store-events/router-store-cancel-event';
-export * from './lib/router-store-events/router-store-error-event';
-export * from './lib/router-store-events/router-store-event';
-export * from './lib/router-store-events/router-store-navigated-event';
-export * from './lib/router-store-events/router-store-navigation-event';
-export * from './lib/router-store-events/router-store-request-event';
+export * from './lib/router-store/router-store-events/router-store-cancel-event';
+export * from './lib/router-store/router-store-events/router-store-error-event';
+export * from './lib/router-store/router-store-events/router-store-event';
+export * from './lib/router-store/router-store-events/router-store-navigated-event';
+export * from './lib/router-store/router-store-events/router-store-navigation-event';
+export * from './lib/router-store/router-store-events/router-store-request-event';

35d1fc84d0bf63bab8cef909e0a64dc1ac75e7a3
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-11-17 23:38:47 +0100

    feat: expose global and local router stores

--- packages/router-component-store/src/index.ts
+++ packages/router-component-store/src/index.ts
@@ -12,3 +12,12 @@ export * from './lib/router-store/router-store-events/router-store-event';
 export * from './lib/router-store/router-store-events/router-store-navigated-event';
 export * from './lib/router-store/router-store-events/router-store-navigation-event';
 export * from './lib/router-store/router-store-events/router-store-request-event';
+
+// GlobalRouterStore
+export * from './lib/global-router-store/global-router-store';
+
+// LocalRouterStore
+export * from './lib/local-router-store/local-router-store';
+
+// Serializable route state
+export { MinimalActivatedRouteSnapshot } from './lib/minimal-router-state-serializer';

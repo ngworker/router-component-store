69bb02bc231a0510ed3847181a9774d50df8eaf9
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-10-19 23:46:14 +0000

    refactor: remove unused `ComponentStore` port of `@ngrx/router-store`

--- packages/router-component-store/src/index.ts
+++ packages/router-component-store/src/index.ts
@@ -1,18 +1,3 @@
-// // RouterStore state
-// export { RouterStore } from './lib/router-store/router-store';
-
-// // RouterStore configuration
-// export { RouterStoreModule } from './lib/router-store/router-store.module';
-// export { RouterStoreConfig } from './lib/router-store/router-store-config';
-
-// // RouterStore events
-// export * from './lib/router-store/router-store-events/router-store-cancel-event';
-// export * from './lib/router-store/router-store-events/router-store-error-event';
-// export * from './lib/router-store/router-store-events/router-store-event';
-// export * from './lib/router-store/router-store-events/router-store-navigated-event';
-// export * from './lib/router-store/router-store-events/router-store-navigation-event';
-// export * from './lib/router-store/router-store-events/router-store-request-event';
-
 // GlobalRouterStore
 export * from './lib/global-router-store/provide-global-router-store';
 

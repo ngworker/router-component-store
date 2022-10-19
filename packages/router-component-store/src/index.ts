288f42e33dccccdcb6323f09cabce137258ba879
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-10-19 23:03:36 +0000

    feat!: replace implementations with `RouterComponentStore` in public API

--- packages/router-component-store/src/index.ts
+++ packages/router-component-store/src/index.ts
@@ -14,10 +14,13 @@
 // export * from './lib/router-store/router-store-events/router-store-request-event';
 
 // GlobalRouterStore
-export * from './lib/global-router-store/global-router-store';
+export * from './lib/global-router-store/provide-global-router-store';
 
 // LocalRouterStore
-export * from './lib/local-router-store/local-router-store';
+export * from './lib/local-router-store/provide-local-router-store';
+
+// RouterComponentStore
+export * from './lib/router-component-store';
 
 // Serializable route state
 export { MinimalActivatedRouteSnapshot } from './lib/@ngrx/router-store/minimal_serializer';

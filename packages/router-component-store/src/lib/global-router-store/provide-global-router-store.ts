e5dd52ab780063c23a8e5aade99248a5a5ae5353
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2022-10-23 22:54:14 +0000

    docs: add inline docs

--- packages/router-component-store/src/lib/global-router-store/provide-global-router-store.ts
+++ packages/router-component-store/src/lib/global-router-store/provide-global-router-store.ts
@@ -2,6 +2,36 @@ import { ClassProvider, Provider } from '@angular/core';
 import { RouterStore } from '../router-store';
 import { GlobalRouterStore } from './global-router-store';
 
+/**
+ * Provide an application-wide router store that can be injected in any class.
+ *
+ * Use this provider factory in a root environment injector.
+ *
+ * @returns The providers required for a global router store.
+ *
+ * @example
+ * // Providing in a standalone Angular application
+ * // main.ts
+ * // (...)
+ * import { provideGlobalRouterStore } from '@ngworker/router-component-store';
+ *
+ * bootstrapApplication(AppComponent, {
+ *   providers: [provideGlobalRouterStore()],
+ * }).catch((error) => console.error(error));
+ *
+ *
+ * @example
+ * // Providing in a classic Angular application
+ * // app.module.ts
+ * // (...)
+ * import { provideGlobalRouterStore } from '@ngworker/router-component-store';
+ *
+ * (@)NgModule({
+ *   // (...)
+ *   providers: [provideGlobalRouterStore()],
+ * })
+ * export class AppModule {}
+ */
 export function provideGlobalRouterStore(): Provider {
   const globalRouterStoreProvider: ClassProvider = {
     provide: RouterStore,
--- packages/router-component-store/src/lib/local-router-store/provide-local-router-store.ts
+++ packages/router-component-store/src/lib/local-router-store/provide-local-router-store.ts
@@ -2,6 +2,35 @@ import { ClassProvider, Provider } from '@angular/core';
 import { RouterStore } from '../router-store';
 import { LocalRouterStore } from './local-router-store';
 
+/**
+ * Provide a component-level router store that can be injected in any directive,
+ * component, pipe, or component-level service.
+ *
+ * Use this provider factory in `Component.providers` or
+ * `Component.viewProviders` to make a local router store available to a
+ * component sub-tree.
+ *
+ * @returns The providers required for a local router store.
+ *
+ * @example
+ * // Providing and injecting in a component
+ * // hero-detail.component.ts
+ * // (...)
+ * import {
+ *   provideLocalRouterStore,
+ *   RouterStore,
+ * } from '@ngworker/router-component-store';
+ *
+ * (@)Component({
+ *   // (...)
+ *   providers: [provideLocalRouterStore()],
+ * })
+ * export class HeroDetailComponent {
+ *   heroId$: Observable<string> = this.routerStore.selectQueryParam('id');
+ *
+ *   constructor(private routerStore: RouterStore) {}
+ * }
+ */
 export function provideLocalRouterStore(): Provider {
   const localRouterStoreProvider: ClassProvider = {
     provide: RouterStore,

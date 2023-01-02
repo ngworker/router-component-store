f3d3382145f3a23aa721b8a2641aefb60716d6f8
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2023-01-02 23:03:33 +0100

    test: cover multiple navigations

--- packages/router-component-store/src/lib/filter-router-event.operator.spec.ts
+++ packages/router-component-store/src/lib/filter-router-event.operator.spec.ts
@@ -1,3 +1,4 @@
+import { Component, NgZone } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import {
   NavigationEnd,
@@ -6,66 +7,158 @@ import {
   RoutesRecognized,
 } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { firstValueFrom, skip } from 'rxjs';
+import { firstValueFrom, take, toArray } from 'rxjs';
 import { filterRouterEvents } from './filter-router-event.operator';
 
+@Component({
+  standalone: true,
+  template: '',
+})
+class DummyTestComponent {}
+
+const navigationEnd = new NavigationEnd(1, '/', '/');
+const navigationStart = new NavigationStart(1, '/');
+const routesRecognized = new RoutesRecognized(
+  1,
+  '/',
+  '/',
+  expect.objectContaining({
+    url: '/',
+  })
+);
+
 describe(filterRouterEvents.name, () => {
-  function setup() {
+  function setup({
+    assertions = 1,
+  }: {
+    readonly assertions?: number;
+  } = {}) {
+    expect.assertions(assertions);
+
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes([])],
+      imports: [
+        RouterTestingModule.withRoutes([
+          {
+            path: 'test',
+            component: DummyTestComponent,
+          },
+        ]),
+      ],
     });
 
+    const ngZone = TestBed.inject(NgZone);
     const router = TestBed.inject(Router);
+    const routerEvents = router.events;
 
     return {
-      router,
+      navigateByUrl(url: string) {
+        return ngZone.run(() => router.navigateByUrl(url));
+      },
+      routerEvents,
     };
   }
 
-  it('filters one router event type', async () => {
-    expect.assertions(1);
-    const { router } = setup();
-    const navigationStart$ = router.events.pipe(
+  it('filters 1 router event type', async () => {
+    const { navigateByUrl, routerEvents } = setup();
+    const navigationStart$ = routerEvents.pipe(
       filterRouterEvents(NavigationStart)
     );
     const whenNavigationStart = firstValueFrom(navigationStart$);
 
-    router.initialNavigation();
+    await navigateByUrl('/');
 
-    await expect(whenNavigationStart).resolves.toBeInstanceOf(NavigationStart);
+    await expect(whenNavigationStart).resolves.toEqual(navigationStart);
   });
 
-  it('filters two router event types', async () => {
-    expect.assertions(2);
-    const { router } = setup();
-    const navigation$ = router.events.pipe(
+  it('filters 2 router event types', async () => {
+    const { navigateByUrl, routerEvents } = setup();
+    const navigation$ = routerEvents.pipe(
       filterRouterEvents(NavigationEnd, NavigationStart)
     );
-    const whenNavigationStart = firstValueFrom(navigation$);
-    const whenNavigationEnd = firstValueFrom(navigation$.pipe(skip(1)));
+    const whenNavigation = firstValueFrom(navigation$.pipe(take(2), toArray()));
 
-    router.initialNavigation();
+    await navigateByUrl('/');
 
-    await expect(whenNavigationStart).resolves.toBeInstanceOf(NavigationStart);
-    await expect(whenNavigationEnd).resolves.toBeInstanceOf(NavigationEnd);
+    await expect(whenNavigation).resolves.toEqual([
+      navigationStart,
+      navigationEnd,
+    ]);
   });
 
-  it('filters three router event types', async () => {
-    expect.assertions(3);
-    const { router } = setup();
-    const navigation$ = router.events.pipe(
+  it('filters 3 router event types', async () => {
+    const { navigateByUrl, routerEvents } = setup();
+    const navigation$ = routerEvents.pipe(
       filterRouterEvents(NavigationEnd, NavigationStart, RoutesRecognized)
     );
-    const whenNavigationStart = firstValueFrom(navigation$);
-    const whenRoutesRecognized = firstValueFrom(navigation$.pipe(skip(1)));
-    const whenNavigationEnd = firstValueFrom(navigation$.pipe(skip(2)));
+    const whenNavigation = firstValueFrom(navigation$.pipe(take(3), toArray()));
 
-    router.initialNavigation();
+    await navigateByUrl('/');
 
-    await expect(whenNavigationStart).resolves.toBeInstanceOf(NavigationStart);
-    await expect(whenRoutesRecognized).resolves.toBeInstanceOf(
-      RoutesRecognized
+    await expect(whenNavigation).resolves.toEqual([
+      navigationStart,
+      routesRecognized,
+      navigationEnd,
+    ]);
+  });
+
+  it('filters multiple events of 1 router event type', async () => {
+    const { navigateByUrl, routerEvents } = setup();
+    const navigationStart$ = routerEvents.pipe(
+      filterRouterEvents(NavigationStart)
     );
-    await expect(whenNavigationEnd).resolves.toBeInstanceOf(NavigationEnd);
+    const whenNavigationStart = firstValueFrom(
+      navigationStart$.pipe(take(2), toArray())
+    );
+
+    await navigateByUrl('/');
+    await navigateByUrl('/test');
+
+    await expect(whenNavigationStart).resolves.toEqual([
+      navigationStart,
+      new NavigationStart(2, '/test'),
+    ]);
+  });
+
+  it('filters multiple events of 2 router event types', async () => {
+    const { navigateByUrl, routerEvents } = setup();
+    const navigation$ = routerEvents.pipe(
+      filterRouterEvents(NavigationEnd, NavigationStart)
+    );
+    const whenNavigation = firstValueFrom(navigation$.pipe(take(4), toArray()));
+
+    await navigateByUrl('/');
+    await navigateByUrl('/test');
+
+    await expect(whenNavigation).resolves.toEqual([
+      navigationStart,
+      navigationEnd,
+      new NavigationStart(2, '/test'),
+      new NavigationEnd(2, '/test', '/test'),
+    ]);
+  });
+
+  it('filters multiple events of 3 router event types', async () => {
+    const { navigateByUrl, routerEvents } = setup();
+    const navigation$ = routerEvents.pipe(
+      filterRouterEvents(NavigationEnd, NavigationStart, RoutesRecognized)
+    );
+    const whenNavigation = firstValueFrom(navigation$.pipe(take(6), toArray()));
+
+    await navigateByUrl('/');
+    await navigateByUrl('/test');
+
+    await expect(whenNavigation).resolves.toEqual([
+      navigationStart,
+      routesRecognized,
+      navigationEnd,
+      new NavigationStart(2, '/test'),
+      new RoutesRecognized(
+        2,
+        '/test',
+        '/test',
+        expect.objectContaining({ url: '/test' })
+      ),
+      new NavigationEnd(2, '/test', '/test'),
+    ]);
   });
 });

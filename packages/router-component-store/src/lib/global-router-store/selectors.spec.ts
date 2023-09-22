9266822aa4d3735fdcd95dc081c8b0ea781d09d0
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2023-09-22 23:47:57 +0200

    test: compare `GlobalRouterStore` selectors to NgRx Router Store selectors and `Router#events`

--- packages/router-component-store/src/lib/global-router-store/selectors.spec.ts
+++ packages/router-component-store/src/lib/global-router-store/selectors.spec.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
-import { NavigationEnd, NavigationStart, Route } from '@angular/router';
+import {
+  NavigationEnd,
+  NavigationStart,
+  Params,
+  Route,
+  Router,
+  UrlSegment,
+} from '@angular/router';
+import {
+  DEFAULT_ROUTER_FEATURENAME,
+  getSelectors,
+  provideRouterStore,
+  routerReducer,
+} from '@ngrx/router-store';
+import { provideStore, Store } from '@ngrx/store';
 import { createFeatureHarness } from '@ngworker/spectacular';
-import { firstValueFrom, take, toArray } from 'rxjs';
+import { filter, firstValueFrom, take, toArray } from 'rxjs';
+import { MinimalActivatedRouteSnapshot } from '../@ngrx/router-store/minimal-activated-route-state-snapshot';
+import { MinimalRouteData } from '../minimal-route-data';
 import { RouterStore } from '../router-store';
 import { GlobalRouterStore } from './global-router-store';
 import { provideGlobalRouterStore } from './provide-global-router-store';
@@ -14,7 +30,7 @@ class DummyAuthComponent {}
 
 describe(`${GlobalRouterStore.name} selectors`, () => {
   function setup({
-    assertions = 1,
+    assertions = 2,
     data = {},
     title,
   }: {
@@ -27,7 +43,14 @@ describe(`${GlobalRouterStore.name} selectors`, () => {
     const featurePath = 'auth';
     const harness = createFeatureHarness({
       featurePath,
-      providers: [provideGlobalRouterStore()],
+      providers: [
+        provideGlobalRouterStore(),
+        // We compare `GlobalRouterStore` to NgRx Router Store selectors
+        provideStore({
+          [DEFAULT_ROUTER_FEATURENAME]: routerReducer,
+        }),
+        provideRouterStore(),
+      ],
       routes: [
         {
           path: featurePath,
@@ -45,11 +68,14 @@ describe(`${GlobalRouterStore.name} selectors`, () => {
 
     return {
       harness,
+      get ngrxRouterStore() {
+        return getSelectors();
+      },
     };
   }
 
   it('exposes a selector for the current route', async () => {
-    const { harness } = setup({
+    const { harness, ngrxRouterStore } = setup({
       data: {
         testData: 'test-data',
       },
@@ -60,9 +86,7 @@ describe(`${GlobalRouterStore.name} selectors`, () => {
       '~/bqbNGrezShfz?ref=ngworker.github.io#test-fragment'
     );
 
-    await expect(
-      firstValueFrom(harness.inject(RouterStore).currentRoute$)
-    ).resolves.toEqual({
+    const expectedRouteSnapshot: MinimalActivatedRouteSnapshot = {
       children: [],
       data: {
         testData: 'test-data',
@@ -80,84 +104,122 @@ describe(`${GlobalRouterStore.name} selectors`, () => {
         title: 'Static title',
       },
       title: 'Static title',
-      url: [
-        {
-          path: 'bqbNGrezShfz',
-          parameters: {},
-        },
-      ],
+      url: [new UrlSegment('bqbNGrezShfz', {})],
+    };
+    await expect(
+      firstValueFrom(harness.inject(RouterStore).currentRoute$)
+    ).resolves.toEqual(expectedRouteSnapshot);
+    await expect(
+      firstValueFrom(
+        harness.inject(Store).select(ngrxRouterStore.selectCurrentRoute)
+      )
+    ).resolves.toEqual({
+      ...expectedRouteSnapshot,
+      // NOTE(@LayZeeDK) Seems to be a bug in NgRx Router Store 15.0.0
+      title: undefined,
     });
   });
 
   it('exposes a selector for the fragment', async () => {
-    const { harness } = setup();
+    const { harness, ngrxRouterStore } = setup();
 
     await harness.router.navigateByUrl(
       '~/FvQBMBzAbNbZ?ref=ngworker.github.io#test-fragment'
     );
 
+    const expectedFragment = 'test-fragment';
     await expect(
       firstValueFrom(harness.inject(RouterStore).fragment$)
-    ).resolves.toBe('test-fragment');
+    ).resolves.toBe(expectedFragment);
+    await expect(
+      firstValueFrom(
+        harness.inject(Store).select(ngrxRouterStore.selectFragment)
+      )
+    ).resolves.toBe(expectedFragment);
   });
 
   it('exposes a selector for query params', async () => {
-    const { harness } = setup();
+    const { harness, ngrxRouterStore } = setup();
 
     await harness.router.navigateByUrl(
       '~/SfBGZmaAHqSn?ref=ngworker.github.io#test-fragment'
     );
 
+    const expectedQueryParams: Params = {
+      ref: 'ngworker.github.io',
+    };
     await expect(
       firstValueFrom(harness.inject(RouterStore).queryParams$)
-    ).resolves.toEqual({
-      ref: 'ngworker.github.io',
-    });
+    ).resolves.toEqual(expectedQueryParams);
+    await expect(
+      firstValueFrom(
+        harness.inject(Store).select(ngrxRouterStore.selectQueryParams)
+      )
+    ).resolves.toEqual(expectedQueryParams);
   });
 
   it('creates a selector for a specific query param', async () => {
-    const { harness } = setup();
+    const { harness, ngrxRouterStore } = setup();
 
     await harness.router.navigateByUrl(
       '~/bSbebKhzfKkg?ref=ngworker.github.io#test-fragment'
     );
 
+    const expectedRef = 'ngworker.github.io';
     await expect(
       firstValueFrom(harness.inject(RouterStore).selectQueryParam('ref'))
-    ).resolves.toBe('ngworker.github.io');
+    ).resolves.toBe(expectedRef);
+    await expect(
+      firstValueFrom(
+        harness.inject(Store).select(ngrxRouterStore.selectQueryParam('ref'))
+      )
+    ).resolves.toBe(expectedRef);
   });
 
   it('exposes a selector for route params', async () => {
-    const { harness } = setup();
+    const { harness, ngrxRouterStore } = setup();
 
     await harness.router.navigateByUrl(
       '~/tBmuJXFZNEdC?ref=ngworker.github.io#test-fragment'
     );
 
+    const expectedRouteParams: Params = {
+      token: 'tBmuJXFZNEdC',
+    };
     await expect(
       firstValueFrom(harness.inject(RouterStore).routeParams$)
-    ).resolves.toEqual({
-      token: 'tBmuJXFZNEdC',
-    });
+    ).resolves.toEqual(expectedRouteParams);
+    await expect(
+      firstValueFrom(
+        harness.inject(Store).select(ngrxRouterStore.selectRouteParams)
+      )
+    ).resolves.toEqual(expectedRouteParams);
   });
 
   it('creates a selector for a specific route param', async () => {
-    const { harness } = setup();
+    const { harness, ngrxRouterStore } = setup();
 
     await harness.router.navigateByUrl(
       '~/huHynaJHbGxU?ref=ngworker.github.io#test-fragment'
     );
 
+    const expectedToken = 'huHynaJHbGxU';
     await expect(
       firstValueFrom(harness.inject(RouterStore).selectRouteParam('token'))
-    ).resolves.toBe('huHynaJHbGxU');
+    ).resolves.toBe(expectedToken);
+    await expect(
+      firstValueFrom(
+        harness.inject(Store).select(ngrxRouterStore.selectRouteParam('token'))
+      )
+    ).resolves.toBe(expectedToken);
   });
 
   it('exposes a selector for route data', async () => {
-    const { harness } = setup({
-      data: {
-        testData: 'test-data',
-      },
+    const expectedRouteData: MinimalRouteData = {
+      testData: 'test-data',
+    };
+    const { harness, ngrxRouterStore } = setup({
+      data: expectedRouteData,
     });
 
     await harness.router.navigateByUrl(
@@ -166,15 +228,19 @@ describe(`${GlobalRouterStore.name} selectors`, () => {
 
     await expect(
       firstValueFrom(harness.inject(RouterStore).routeData$)
-    ).resolves.toEqual({
-      testData: 'test-data',
-    });
+    ).resolves.toEqual(expectedRouteData);
+    await expect(
+      firstValueFrom(
+        harness.inject(Store).select(ngrxRouterStore.selectRouteData)
+      )
+    ).resolves.toEqual(expectedRouteData);
   });
 
   it('creates a selector for specific route data', async () => {
-    const { harness } = setup({
+    const expectedTestData = 'test-data';
+    const { harness, ngrxRouterStore } = setup({
       data: {
-        testData: 'test-data',
+        testData: expectedTestData,
       },
     });
 
@@ -186,24 +252,37 @@ describe(`${GlobalRouterStore.name} selectors`, () => {
       firstValueFrom(
         harness.inject(RouterStore).selectRouteData<string>('testData')
       )
-    ).resolves.toBe('test-data');
+    ).resolves.toBe(expectedTestData);
+    await expect(
+      firstValueFrom(
+        harness.inject(Store).select(ngrxRouterStore.selectRouteData)
+      )
+    ).resolves.toEqual({
+      testData: expectedTestData,
+    });
   });
 
   it('exposes a selector for the URL', async () => {
-    const { harness } = setup();
+    const { harness, ngrxRouterStore } = setup();
 
     await harness.router.navigateByUrl(
       '~/yGEJHVwByWWN?ref=ngworker.github.io#test-fragment'
     );
 
+    const expectedUrl =
+      '/auth/yGEJHVwByWWN?ref=ngworker.github.io#test-fragment';
     await expect(
       firstValueFrom(harness.inject(RouterStore).url$)
-    ).resolves.toBe('/auth/yGEJHVwByWWN?ref=ngworker.github.io#test-fragment');
+    ).resolves.toBe(expectedUrl);
+    await expect(
+      firstValueFrom(harness.inject(Store).select(ngrxRouterStore.selectUrl))
+    ).resolves.toBe(expectedUrl);
   });
 
   it('exposes a selector for the route title that emits static route titles', async () => {
-    const { harness } = setup({
-      title: 'Static title',
+    const expectedTitle = 'Static title';
+    const { harness, ngrxRouterStore } = setup({
+      title: expectedTitle,
     });
 
     await harness.router.navigateByUrl(
@@ -212,13 +291,17 @@ describe(`${GlobalRouterStore.name} selectors`, () => {
 
     await expect(
       firstValueFrom(harness.inject(RouterStore).title$)
-    ).resolves.toBe('Static title');
+    ).resolves.toBe(expectedTitle);
+    await expect(
+      firstValueFrom(harness.inject(Store).select(ngrxRouterStore.selectTitle))
+    ).resolves.toBe(expectedTitle);
   });
 
   it('exposes a selector for the route title that emits resolved route titles', async () => {
-    const { harness } = setup({
+    const expectedTitle = 'test-data';
+    const { harness, ngrxRouterStore } = setup({
       data: {
-        testData: 'test-data',
+        testData: expectedTitle,
       },
       title: (route) => route.data['testData'],
     });
@@ -229,7 +312,13 @@ describe(`${GlobalRouterStore.name} selectors`, () => {
 
     await expect(
       firstValueFrom(harness.inject(RouterStore).title$)
-    ).resolves.toBe('test-data');
+    ).resolves.toBe(expectedTitle);
+    await expect(
+      firstValueFrom(harness.inject(Store).select(ngrxRouterStore.selectTitle))
+    ).resolves.toBe(
+      // NOTE(@LayZeeDK) Seems to be a bug in NgRx Router Store 15.0.0
+      undefined
+    );
   });
 
   it('exposes a selector for specific router events', async () => {
@@ -240,12 +329,27 @@ describe(`${GlobalRouterStore.name} selectors`, () => {
       .inject(RouterStore)
       .selectRouterEvents(NavigationStart, NavigationEnd);
     const whenNavigation = firstValueFrom(navigation$.pipe(take(2), toArray()));
+    const angularRouterNavigation$ = harness
+      .inject(Router)
+      .events.pipe(
+        filter(
+          (event): event is NavigationStart | NavigationEnd =>
+            event instanceof NavigationStart || event instanceof NavigationEnd
+        )
+      );
+    const whenAngularRouterNavigation = firstValueFrom(
+      angularRouterNavigation$.pipe(take(2), toArray())
+    );
 
     await harness.router.navigateByUrl(expectedUrl);
 
-    await expect(whenNavigation).resolves.toEqual([
+    const expectedNavigation: [NavigationStart, NavigationEnd] = [
       new NavigationStart(2, expectedUrl),
       new NavigationEnd(2, expectedUrl, expectedUrl),
-    ]);
+    ];
+    await expect(whenNavigation).resolves.toEqual(expectedNavigation);
+    await expect(whenAngularRouterNavigation).resolves.toEqual(
+      expectedNavigation
+    );
   });
 });
